2372bd114c6315c72050da84851b91afdd934c33
Annie1790 <budayaniko@outlook.com>
2023-08-20 10:57:23 +0100

    another minor size adjusment....

--- src/components/recipes/RecipeCategoryButtons.jsx
+++ src/components/recipes/RecipeCategoryButtons.jsx
@@ -1,7 +1,7 @@
 const RecipeCategoryButtons = () => {
 
     return (
-        <div className="px-4 overflow-x-scroll no-scrollbar">
+        <div className="px-4">
             <button className="text-xl m-0.5 p-1 border border-gray-400 rounded-full   ">All</button>
             <button className="text-xl m-0.5 p-1 border border-gray-400 rounded-full   ">⭐ Favorites</button>
             <button className="text-xl m-0.5 p-1 border border-gray-400 rounded-full   ">🍲 Soup</button>
